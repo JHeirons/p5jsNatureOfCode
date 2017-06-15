4fbda92ce4a8ec1485191259e041223d0c260b6b
JHeirons <j.r.heirons@gmail.com>
2017-06-15 22:05:23 +0100

    colour as z

--- perlinNoise/sketch.js
+++ perlinNoise/sketch.js
@@ -17,18 +17,20 @@ function draw() {
 
 function Walker() {
   this.position = createVector(width/2,height/2);
-  this.noff = createVector(random(1000),random(1000));
+  this.noff = createVector(random(1000),random(1000), random(1000));
+    this.col;
 
   this.display = function() {
     strokeWeight(2);
-    fill(51);
-    stroke(0);
+    fill(this.col);
+    noStroke(0);
     ellipse(this.position.x, this.position.y, 48, 48);
   };
 
   this.walk = function() {
     this.position.x = map(noise(this.noff.x),0,1,0,width);
     this.position.y = map(noise(this.noff.y),0,1,0,height);
-    this.noff.add(0.01,0.02,0);
+    this.col = map(noise(this.noff.z),0,1,0,255);
+    this.noff.add(0.004,0.005,0.005);
   };
 }
