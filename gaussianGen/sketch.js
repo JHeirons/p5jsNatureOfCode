6d20ab18a382ff77078bb5a86718ec9cd2d34040
JHeirons <j.r.heirons@gmail.com>
2017-06-15 19:51:23 +0100

    gaussian

--- gaussianGen/sketch.js
+++ gaussianGen/sketch.js
@@ -1,20 +1,26 @@
-var distribution = new Array(500);
+var distribution = new Array(1000);
 
 function setup() {
   createCanvas(600, 400);
-  for (var i = 0; i < distribution.length; i++) {
-    distribution[i] = floor(randomGaussian(0,150));
-  }
+  
 }
 
 function draw() {
   background(255);
+    
+   for (var i = 0; i < distribution.length; i++) {
+    distribution[i] = floor(randomGaussian(0,150));
+  } 
+    
  translate(width/2, height/2);
  for (var i = 0; i < distribution.length; i++) {
     rotate(TWO_PI/distribution.length);
     stroke(0);
+    fill(random(255), random(255), random(255));
     var dist = abs(distribution[i]);
-    line(0, 0, dist, 0);
+    rect(0, 0, dist, 2);
+    
   }
+
 }
 
