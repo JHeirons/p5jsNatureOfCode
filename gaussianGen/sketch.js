64bb192f06ba98b54b898f4b07f552f8eb515af1
JHeirons <j.r.heirons@gmail.com>
2017-06-15 20:15:02 +0100

    line cirles

--- gaussianGen/sketch.js
+++ gaussianGen/sketch.js
@@ -1,26 +1,22 @@
-var distribution = new Array(1000);
+
 
 function setup() {
   createCanvas(600, 400);
-  
+  smooth();
 }
 
 function draw() {
-  background(255);
+    background(255);
     
-   for (var i = 0; i < distribution.length; i++) {
-    distribution[i] = floor(randomGaussian(0,150));
-  } 
+    var xloc = randomGaussian();
+    var sd = 70;
+    var mean = width/2;
+    xloc = (xloc * sd) + mean;
     
- translate(width/2, height/2);
- for (var i = 0; i < distribution.length; i++) {
-    rotate(TWO_PI/distribution.length);
-    stroke(0);
-    fill(random(255), random(255), random(255));
-    var dist = abs(distribution[i]);
-    rect(0, 0, dist, 2);
-    
-  }
+    noStroke();
+    fill(0);
+    ellipse(xloc, height/2, 20, 20);
+   
 
 }
 
